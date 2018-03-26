459e0137d47223f3980a87e4d191f8ec7a4b5222
demx8as6 <martin.skorupski@highstreet-technologies.com>
2018-03-26 11:20:14 +0200

    init measurement

---
+++ createPnf.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+. config
+# search at winlab for examples        
---
+++ send15minPm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+pnfType=$1
+alarmType=$2
+severity=$3
+eventId=$(uuidgen)
+. config
+
+sed -e "s/@pnfId@/${pnfIdByType[$pnfType]}/g; s/@eventId@/${eventId}/g; s/@controllerId@/${controllerId}/g; s/@controllerName@/${controllerName}/g; s/@pnfType@/${pnfType}/g; s/@interfaceId@/${interfaceByType[$pnfType]}/g; s/@alarmType@/${alarmType}/g; s/@severity@/${severity}/g; s/\"@timestamp@\"/$(($(date -u +%s%N)/1000000))/g; " ./json/measurement-body-template.json > ./json/measurement-body.json
+
+echo
+echo "  controllerId: ${controllerId}"
+echo "controllerName: ${controllerName}"
+echo "         pnfId: ${pnfIdByType[$pnfType]}"
+echo "          type: ${pnfType}"
+echo "        vendor: ${vendorsByType[$pnfType]}"
+echo "     interface: ${interfaceByType[$pnfType]}"
+echo "         alarm: ${alarmType}"
+echo "     timestamp: $(($(date -u +%s%N)/1000000))"
+echo "       eventId: ${eventId}"
+echo 
+# json=$(< ./json/measurement-body.json)
+# echo "     body: $json"
+# echo
+
+curl -i -u $basicAuthVes -X POST -d  @json/measurement-body.json --header "Content-Type: application/json" $dcaeUrl
