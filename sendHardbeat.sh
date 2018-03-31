3a5a41d1d616e1d552d970bccbd7fe8ca20eaccb
demx8as6 <martin.skorupski@highstreet-technologies.com>
2018-03-31 19:35:30 +0200

    Adoptation of new features and feedback

--- sendHardbeat.sh
+++ sendHardbeat.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
+################################################################################
+# Script to send an VES Message Event to DCAE
 
-eventId=$(uuidgen)
-. config
+   timestamp=$(date -u +%s%3N);
+   eventTime=$(date -u -d @${timestamp:0:$((${#timestamp}-3))} +'%Y-%m-%dT%H:%M:%S').${timestamp:(-3)}" UTC";
+     eventId=$(uuidgen);
+       spaces="                ";
+     sequence=;
+. config;
 
-sed -e "s/@pnfId@/${pnfIdByType[$pnfType]}/g; s/@eventId@/${eventId}/g; s/@controllerId@/${controllerId}/g; s/@controllerName@/${controllerName}/g; s/@pnfType@/${pnfType}/g; s/@interfaceId@/${interfaceByType[$pnfType]}/g; s/@alarmType@/${alarmType}/g; s/@severity@/${severity}/g; s/\"@timestamp@\"/$(($(date -u +%s%N)/1000000))/g; " ./json/heartbeat-body-template.json > ./json/heartbeat-body.json
+declare -A mapping=(
+    [controllerId]=${controllerId}
+    [controllerName]=${controllerName}
+    [eventId]=${eventId}
+    [timestamp]=${timestamp}
+    [eventTime]=${eventTime}
+)
 
-echo
-echo "  controllerId: ${controllerId}"
-echo "controllerName: ${controllerName}"
-echo "     timestamp: $(($(date -u +%s%N)/1000000))"
-echo "       eventId: ${eventId}"
-echo 
-# json=$(< ./json/heartbeat-body.json)
+echo;
+for key in "${!mapping[@]}"
+do
+  #label=${${"$spaces$i"}:(-14)};
+  label=$spaces$key;
+  label=${label:(-16)};
+  echo "$label: ${mapping[$key]}";
+  sequence="$sequence s/@$key@/${mapping[$key]}/g; "
+done
+echo;
+
+sed -e "$sequence" ./json/heartbeat-body-template.json > ./json/heartbeat-body.json
+
+# json=$(< ./json/fault-body.json)
 # echo "     body: $json"
 # echo
 
-curl -i -u $basicAuthVes -X POST -d  @json/heartbeat-body.json --header "Content-Type: application/json" $dcaeUrl
+curl -i -u $basicAuthVes -X POST -d  @json/heartbeat-body.json --header "Content-Type: application/json" $urlVes
